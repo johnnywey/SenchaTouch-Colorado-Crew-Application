6dd6ef5799cd4b96f70f15a64dbe4cb92a2e47a7
Johnny Wey <johnnywey@gmail.com>
2011-03-21 21:43:21 -0600

    Update CSS.

--- app/views/GroupList.js
+++ app/views/GroupList.js
@@ -1,52 +1,61 @@
 /**
  * @class colocrew.views.GroupList
  * @extends Ext.Panel
- * 
+ *
  */
 colocrew.views.GroupList = Ext.extend(Ext.Panel, {
     layout: 'fit',
-    dockedItems: [{
-        dock: 'top',
-        xtype: 'toolbar',
-        title: 'Crew',
-        ui: 'dark'
-    },{
-        dock: 'top',
-        xtype: 'toolbar',
-        ui: 'light',
-        title: 'Chose a Group',
-        items: [{
-            text: 'Categories',
-            ui: 'back',
-            listeners: {
-                'tap': function () {
-                    Ext.dispatch({
-                        controller: colocrew.controllers.crew,
-                        action: 'listCategory',
-                        animation: {type:'slide', direction:'right'}
-                        });
+    dockedItems: [
+        {
+            dock: 'top',
+            xtype: 'toolbar',
+            title: 'Crew',
+            ui: 'dark',
+            cls: 'top-bar'
+        },
+        {
+            dock: 'top',
+            xtype: 'toolbar',
+            ui: 'light',
+            cls: 'top-bar',
+            title: 'Chose a Group',
+            items: [
+                {
+                    text: 'Categories',
+                    ui: 'back',
+                    listeners: {
+                        'tap': function () {
+                            Ext.dispatch({
+                                controller: colocrew.controllers.crew,
+                                action: 'listCategory',
+                                animation: {type:'slide', direction:'right'}
+                            });
+                        }
                     }
                 }
-            }]
-    }],
-
-    items: [{
-        xtype: 'list',
-        cls: "roundList",
-        emptyText : 'No categories on file',
-        title: 'Select a Group',
-        margin: 15,
-        itemTpl: '<tpl if="secondaryCategoryId == 0"><strong>{secondaryCategoryName}</strong></tpl><tpl if="secondaryCategoryId &gt; 0">{secondaryCategoryName}</tpl>',
-        store: "MatchingGroupsStore",
-        onItemDisclosure: function (record) {
-            Ext.dispatch({
-                controller: colocrew.controllers.crew,
-                action: 'listPeople',
-                record: record
-            });
+            ]
         }
-    }],
-    
+    ],
+
+    items: [
+        {
+            xtype: 'list',
+            cls: "roundList",
+            emptyText : 'No categories on file',
+            title: 'Select a Group',
+            margin: 15,
+            itemTpl: '<tpl if="secondaryCategoryId == 0"><strong>{secondaryCategoryName}</strong></tpl><tpl if="secondaryCategoryId &gt; 0">{secondaryCategoryName}</tpl>',
+            store: "MatchingGroupsStore",
+            onItemDisclosure: function (record) {
+                Ext.dispatch({
+                    controller: colocrew.controllers.crew,
+                    action: 'listPeople',
+                    record: record
+                });
+            }
+        }
+    ],
+
     scroll: 'vertical',
 
     initComponent: function() {
